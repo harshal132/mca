a326697bb67db22c87bd8dff06a90a8c93f59fe6
harshal132 <harshalchavan132@outlook.com>
2021-02-11 12:00:10 +0530

    append details in file

--- semester-1/web-technologies/write-file.js
+++ semester-1/web-technologies/write-file.js
@@ -1,4 +1,5 @@
 var fs=require("fs")
+//Create and Write Files
 fs.writeFile("writeFile.txt","Hi, I am writing this content through js.",function (err,data){
     if(err){
         console.log(err,data);
@@ -8,4 +9,13 @@ fs.writeFile("writeFile.txt","Hi, I am writing this content through js.",functio
     }
 }
 );
-
+//Append Files
+fs.appendFile("writeFile.txt","\nUpdate Files - New Content",function (err,data){
+    if(err){
+        console.log(err,data);
+    }
+    else{
+        console.log("File updated successfully");
+    }
+}
+);
