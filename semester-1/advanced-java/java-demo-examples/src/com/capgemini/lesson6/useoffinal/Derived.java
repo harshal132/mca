fe9402412925864729103a7549015a3ac23a81b6
harshal132 <harshalchavan132@outlook.com>
2021-03-06 15:50:10 +0530

    added example files

---
+++ semester-1/advanced-java/java-demo-examples/src/com/capgemini/lesson6/useoffinal/Derived.java
@@ -0,0 +1,13 @@
+package com.capgemini.lesson6.useoffinal;
+
+/*
+ * Below code will not work 
+ * as class Base is declared as final
+ 
+public class Derived extends Base {
+
+}*/
+
+public class Derived {
+
+}
