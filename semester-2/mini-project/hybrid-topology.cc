412517f748c5d460040aed9022a4c1cddde95791
Harshal Chavan <harshalchavan132@outlook.com>
2021-08-15 16:11:41 +0530

    script updates

--- semester-2/mini-project/hybrid-topology.cc
+++ semester-2/mini-project/hybrid-topology.cc
@@ -1,17 +1,19 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
-Mode:C++; c file style:"gnu"; indent tabs mode:nil;
-/*
-* This program is free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License version 2 as
-* published by the Free Software
-* This program is distributed in the hope that it will be
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more
-* You should have received a copy of the GNU General Public License
-* along with this program; if not, write to the Free Software
-* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 1307 USA
-*/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+ 
 // Hybrid_Topology_Mini_Project (Point To Point + Bus Topology)
 // Authors: Harshal Chavan(202124) & Anagha Dongre(202112)
 // Divison: A
@@ -24,6 +26,8 @@ Mode:C++; c file style:"gnu"; indent tabs mode:nil;
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/netanim-module.h"
+#include "ns3/mobility-module.h"
 
 // Default Network Topology
 //
@@ -32,96 +36,100 @@ Mode:C++; c file style:"gnu"; indent tabs mode:nil;
 //    point-to-point  |    |    |    |
 //                    ================
 //                      LAN 10.1.2.0
-#include "ns3/netanim-module.h"
-#include "ns3/mobility-module.h"
+
+
 using namespace ns3;
-NS_LOG_COMPONENT_DEFINE ("Hybrid_Topology_Mini_Project");
 
-int main (int argc, char *argv[]) {
-	bool verbose = true;
-	uint32_t nCsma = 3;
+NS_LOG_COMPONENT_DEFINE ("Mini-Project");
 
-	CommandLine cmd;
-	cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices",nCsma);
-	cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
+int 
+main (int argc, char *argv[])
+{
+  bool verbose = true;
+  uint32_t nCsma = 3;
 
-	cmd.Parse (argc, argv);
+  CommandLine cmd;
+  cmd.AddValue ("nCsma", "Number of \"extra\" CSMA nodes/devices", nCsma);
+  cmd.AddValue ("verbose", "Tell echo applications to log if true", verbose);
 
-	if (verbose)
-	  {
-		LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
-		LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
-	  }
+  cmd.Parse (argc,argv);
 
-	nCsma = nCsma == 0 ? 1 : nCsma;
-	NodeContainer p2pNodes;
-	p2pNodes.Create (2);
-	
-	NodeContainer csmaNodes;
-	csmaNodes.Add (p2pNodes.Get (1));
-	csmaNodes.Create (nCsma);
-	
-	PointToPointHelper pointToPoint;
-	pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-	pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+  if (verbose)
+    {
+      LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
+      LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+    }
 
-	NetDeviceContainer p2pDevices;
-	p2pDevices = pointToPoint.Install (p2pNodes);
-	
-	CsmaHelper csma;
-	csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
-	csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
+  nCsma = nCsma == 0 ? 1 : nCsma;
 
-	NetDeviceContainer csmaDevices;
-	csmaDevices = csma.Install (csmaNodes);
+  NodeContainer p2pNodes;
+  p2pNodes.Create (2);
 
-	InternetStackHelper stack;
-	stack.Install (p2pNodes.Get (0));
-	stack.Install (csmaNodes);
-	
-	Ipv4AddressHelper address;
-	address.SetBase ("10.1.1.0", "255.255.255.0");
-	Ipv4InterfaceContainer p2pInterfaces;
-	p2pInterfaces = address.Assign (p2pDevices);
+  NodeContainer csmaNodes;
+  csmaNodes.Add (p2pNodes.Get (1));
+  csmaNodes.Create (nCsma);
 
-	address.SetBase ("10.1.2.0", "255.255.255.0");
-	Ipv4InterfaceContainer csmaInterfaces;
-	csmaInterfaces = address.Assign (csmaDevices);
+  PointToPointHelper pointToPoint;
+  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
+  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
 
-	UdpEchoClientHelper echoClient (csmaInterfaces.GetAddress (nCsma), 9);
-	echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
-	echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
-	echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+  NetDeviceContainer p2pDevices;
+  p2pDevices = pointToPoint.Install (p2pNodes);
 
-	ApplicationContainer clientApps = echoClient.Install (p2pNodes.Get (0));
-	clientApps.Start (Seconds (2.0));
-	clientApps.Stop (Seconds (10.0));
+  CsmaHelper csma;
+  csma.SetChannelAttribute ("DataRate", StringValue ("100Mbps"));
+  csma.SetChannelAttribute ("Delay", TimeValue (NanoSeconds (6560)));
 
-	Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+  NetDeviceContainer csmaDevices;
+  csmaDevices = csma.Install (csmaNodes);
 
-	pointToPoint.EnablePcapAll ("second");
-	csma.EnablePcap ("second", csmaDevices.Get (1), true);
-	
-	//For Net Anim
-	MobilityHelper mobility;
-	mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
-	mobility.Install(p2pNodes);
-	mobility.Install(csmaNodes);
-	AnimationInterface anim("mini-project-hybrid-topology.xml");
-	AnimationInterface::SetC
-	onstantPosition (p2pNodes.Get(0), 10, 25);
-	AnimationInterface ::SetConstantPosition(p2pNodes.Get(1), 20,25);
-	AnimationInterface ::SetConstantPosition(csmaNodes.Get(1),40,25);
-	AnimationInterface ::SetConstantPosition(csmaNodes.Get(2),50,25);
-	AnimationInterf
-	ace ::SetConstantPosition(csmaNodes.Get(3),60,25);
-	anim.EnablePacketMetadata(true);
+  InternetStackHelper stack;
+  stack.Install (p2pNodes.Get (0));
+  stack.Install (csmaNodes);
 
-	Simulator::Run ();
-	Simulator::Destroy ();
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer p2pInterfaces;
+  p2pInterfaces = address.Assign (p2pDevices);
 
-	return 0;
+  address.SetBase ("10.1.2.0", "255.255.255.0");
+  Ipv4InterfaceContainer csmaInterfaces;
+  csmaInterfaces = address.Assign (csmaDevices);
+
+  UdpEchoServerHelper echoServer (9);
+
+  ApplicationContainer serverApps = echoServer.Install (csmaNodes.Get (nCsma));
+  serverApps.Start (Seconds (1.0));
+  serverApps.Stop (Seconds (10.0));
+
+  UdpEchoClientHelper echoClient (csmaInterfaces.GetAddress (nCsma), 9);
+  echoClient.SetAttribute ("MaxPackets", UintegerValue (1));
+  echoClient.SetAttribute ("Interval", TimeValue (Seconds (1.0)));
+  echoClient.SetAttribute ("PacketSize", UintegerValue (1024));
+
+  ApplicationContainer clientApps = echoClient.Install (p2pNodes.Get (0));
+  clientApps.Start (Seconds (2.0));
+  clientApps.Stop (Seconds (10.0));
+
+  Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+  pointToPoint.EnablePcapAll ("mini-project-hybrid-topology");
+  csma.EnablePcap ("mini-project-hybrid-topology", csmaDevices.Get (1), true);
+  
+        //For Net Anim
+        MobilityHelper mobility;
+        mobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+        mobility.Install(p2pNodes);
+        mobility.Install(csmaNodes);
+        AnimationInterface anim("mini-project-hybrid-topology.xml");
+        AnimationInterface::SetConstantPosition (p2pNodes.Get(0), 10, 25);
+        AnimationInterface::SetConstantPosition(p2pNodes.Get(1), 20,25);
+        AnimationInterface::SetConstantPosition(csmaNodes.Get(1),40,25);
+        AnimationInterface::SetConstantPosition(csmaNodes.Get(2),50,25);
+        AnimationInterface::SetConstantPosition(csmaNodes.Get(3),60,25);
+        anim.EnablePacketMetadata(true);
+
+  Simulator::Run ();
+  Simulator::Destroy ();
+  return 0;
 }
-
-
-
